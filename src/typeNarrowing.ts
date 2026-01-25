21b39f9a424752ef1294eebb89c9bd05eb447526
rajsingh1301 <rjss2829singh@gmail.com>
2026-01-25 22:14:11 +0530

    adf

--- src/typeNarrowing.ts
+++ src/typeNarrowing.ts
@@ -4,3 +4,10 @@ function getChai(kind : string|number|boolean){
 }
 }
 getChai("masala")
+function printId(id: number | string) {
+    if (typeof id === "string") {
+        console.log(id.toUpperCase());
+    } else {
+        console.log(id);
+    }
+}
