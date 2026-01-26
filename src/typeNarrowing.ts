6c9b2f63cecfed4cd73daf92323632c270963e08
rajsingh1301 <rjss2829singh@gmail.com>
2026-01-26 23:05:22 +0530

    adf

--- src/typeNarrowing.ts
+++ src/typeNarrowing.ts
@@ -11,3 +11,18 @@ function printId(id: number | string) {
         console.log(id);
     }
 }
+
+class kulhadChai {
+    Serve(){
+        return `Serving chai in kulhad`
+    }
+}
+class glassChai{   
+    serve(){
+        return `Serving chai in glass`
+    }
+}
+ function serveChai(chai : kulhadChai | glassChai){
+ if(chai )
+ }
+ 
