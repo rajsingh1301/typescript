8f4bcd59ac511a3d71247c682111d02dce85c348
rajsingh1301 <rjss2829singh@gmail.com>
2026-02-16 23:41:14 +0530

    first commit

--- src/typeNarrowing.ts
+++ src/typeNarrowing.ts
@@ -88,3 +88,11 @@ function orderChai(chai : kulhadChai | glassChai){
         console.log("Unknown chai type");
     }
 }
+function orderChaiWithTypeGuard(chai : kulhadChai | glassChai){
+    if(chai instanceof kulhadChai){
+        console.log(chai.Serve());
+    } else if(chai instanceof glassChai){
+        console.log(chai.serve());
+    } else {
+        console.log("Unknown chai type");
+    }
