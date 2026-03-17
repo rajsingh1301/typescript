9686a6e48c4126f17d9a5972c39ad619fa9a322c
rajsingh1301 <rjss2829singh@gmail.com>
2026-03-17 23:59:49 +0530

    starting a new safe branch

--- src/typeNarrowing.ts
+++ src/typeNarrowing.ts
@@ -1,10 +1,4 @@
-function getChai(kind : string|number|boolean){
-    if(typeof kind === "string") {
-        console.log(`chai ordered of ${kind}`)
-}
-}
-getChai("masala")
-function printId(id: number | string) {
+f{
     if (typeof id === "string") {
         console.log(id.toUpperCase());
     } else {
